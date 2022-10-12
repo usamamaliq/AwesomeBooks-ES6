f89a981ffc512c8163ab0221b66af9fb0a4c6be2
Usama Masood <usamamaliq@gmail.com>
2022-10-12 18:26:14 +0500

    Refactored awesome books with module and ES6

---
+++ modules/dynamic.js
@@ -0,0 +1,29 @@
+export const displayBooks = document.querySelector('#display_list');
+export const displayForm = document.querySelector('#display_form');
+export const contact = document.querySelector('#contact');
+export const displayContact = document.querySelector('#display_contact');
+
+export const addBookList = document.querySelector('#form');
+
+const bookItems = document.querySelector('#book-list');
+const awesomeHeading = document.querySelector('#awesome');
+
+export const showOnlyForm = () => {
+  addBookList.style.display = 'flex';
+  awesomeHeading.style.display = 'none';
+  bookItems.style.display = 'none';
+  contact.style.display = 'none';
+};
+export const showBooksOnly = () => {
+  addBookList.style.display = 'none';
+  contact.style.display = 'none';
+  awesomeHeading.style.display = 'block';
+  bookItems.style.display = 'flex';
+};
+
+export function showOnlyContact() {
+  addBookList.style.display = 'none';
+  awesomeHeading.style.display = 'none';
+  bookItems.style.display = 'none';
+  contact.style.display = 'flex';
+}
